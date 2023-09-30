42790808cb82a9e63ce286105acee35779673640
sathishkumar-sathish <146317947+sathishkumar-sathish@users.noreply.github.com>
2023-09-30 13:39:07 +0200

    Create pandas.py

---
+++ pandas.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+ 
+dataset = pd.read_excel("HousePricePrediction.xlsx")
+ 
+# Printing first 5 records of the dataset
+print(dataset.head(5))
